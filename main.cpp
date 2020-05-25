ab4da59e4cb1fca6953e56c425607f7edc5bd72a
Lazar M. Cvetković <jugosoft@hotmail.com>
2020-05-25 14:38:53 +0200

    Gordon Newell Analytics Works

--- main.cpp
+++ main.cpp
@@ -6,14 +6,34 @@
 using namespace std;
 using namespace arma;
 
+void writeOutput(ofstream& ofstream, const mat& matrix, int K)
+{
+    ofstream << "////////////////////////////////////////" << endl;
+    ofstream << "////////////////" << " K := " << K << " ////////////////" << endl;
+    ofstream << "////////////////////////////////////////" << endl;
+
+    ofstream << endl;
+
+    for (int i = 0; i < matrix.n_rows; i++)
+        ofstream << "X_" << i + 1 << " = " << matrix(i, 0) << endl;
+
+    ofstream << endl;
+}
+
 int main(int argc, char** argv)
 {
-    //for (int k = 2; k <= 8; k++) 
-    //{
-        GordonNewellSolver gn(2);
-        mat result = gn.GetXMatrix();
-    //}
+    ofstream outputStream;
+    outputStream.open("potraznje_analiticki.txt");
 
+    for (int k = 2; k <= 8; k++) 
+    {
+        GordonNewellSolver gn(k);
+        mat result = gn.GetXMatrix();
+
+        writeOutput(outputStream, result, k);
+    }
+
+    outputStream.close();
 
 
 
